e91d46a2c36d8a1dbea8b8c841f34042a1e1b3e8
Will Migdol <will.migdol@gmail.com>
2023-08-23 20:50:49 -0700

    added new game to nav

--- src/lib/Components/GamePath.svelte
+++ src/lib/Components/GamePath.svelte
@@ -26,10 +26,12 @@
 			{#if actor}
 				<MediaSelector idx={Math.floor(index / 2)} {actor} imgCSS={IMGCSS} />
 			{/if}
-		{:else if actor}
-			<ActorCard {actor} imgCSS={IMGCSS} />
 		{:else}
-			<ActorSelector actor={actor} index={Math.floor(index / 2)} imgCSS={IMGCSS}/>
+			{#if index > 1}
+				<ActorSelector actor={actor} index={Math.floor(index / 2)} imgCSS={IMGCSS}/>
+			{:else}
+				<ActorCard actor={actor} imgCSS={IMGCSS} />
+			{/if}
 		{/if}
 	{/key}
 </div>
